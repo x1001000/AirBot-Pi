b5ae859fd8d02576b0df306d70931ca2c99ef302
x1001000 <x1001000@gmail.com>
2018-10-07 01:21:58 +0800

    PPD42

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Wiring
+- PPD42 signal to GPIO2(Pin3)
 - DHT22 signal to GPIO4(Pin7)
 
 ## Installing
--- poll_sensors.py
+++ poll_sensors.py
@@ -1,5 +1,9 @@
+import grovepi
+new_val, lowpulseoccupancy = grovepi.dustSensorRead()
+
 import Adafruit_DHT as dht
 humidity, temperature = dht.read_retry(dht.DHT22, 4)
 
 if __name__ == '__main__':
-    print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    print('PM2.5=', lowpulseoccupancy)
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
