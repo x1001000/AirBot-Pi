51fbae55e0bf055608145dcd05d24a8904d54907
isekai <brian1689878638@gmail.com>
2018-10-07 13:28:19 +0800

    www

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AirBot_RPi',
+    version='.SmartCityHackathon.beta.',
+    packages=['airboxpi', 'airboxpi.sensors'],
+    url='https://github.com/x1001000/AirBot-Pi',
+    license='MIT',
+    author='',
+    author_email='',
+    description='a open source airbox made with RPi that have Temperature, humidity  and PM2.5 sensors'
+)
