79c0379263fdbea5081fbe3fef5cdc1ea48f9304
isekai <brian1689878638@gmail.com>
2018-10-07 11:29:22 +0800

    complete app.py

--- airboxpi/sensors/DHT22.py
+++ airboxpi/sensors/DHT22.py
@@ -3,4 +3,5 @@ def t_h():
     return dht.read_retry(dht.DHT22, 4)
 
 if __name__ == '__main__':
-    print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(*t_h()))
+    t, h = t_h()
+    print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
